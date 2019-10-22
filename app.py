8f835f139a87ceb164800214e584d25ea9271484
mholankar <manali.holankar@gmail.com>
2019-10-22 00:38:43 -0700

    Add files via upload
    Added query/filter support

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 from flask import Flask
-from flask import request, jsonify
-from app_lib import read_users_file, read_group_file
+from flask import request, jsonify, abort
+from app_lib import read_users_file, read_group_file, read_users_by_query, read_group_by_query, read_group_by_gid_list
 
 app = Flask(__name__)
 
@@ -12,9 +12,35 @@ def user_service():
     return jsonify(users_list)
 
 
+@app.route('/users/<req_uid>', methods=['GET'])
+def user_service_for_uid(req_uid):
+    users_list = read_users_by_query(req_uid=req_uid)
+    if not users_list:
+        abort(404)
+    return jsonify(users_list)
+
+
+@app.route('/users/<req_uid>/groups', methods=['GET'])
+def group_service_for_user(req_uid):
+    users_list = read_users_by_query(req_uid=req_uid)
+    gids = {}
+    for each_user in users_list:
+        gids.add(each_user['gid'])
+    group_list = read_group_by_gid_list(list(gids))
+    return jsonify(group_list)
+
+
 @app.route('/group', methods=['GET'])
 def group_service():
     group_list = read_group_file()
+    return jsonify(group_list)
+
+
+@app.route('/group/<req_gid>', methods=['GET'])
+def group_service_for_gid(req_gid):
+    group_list = read_group_by_query(req_gid=req_gid)
+    if not group_list:
+        abort(404)
     return jsonify(group_list)
 
 
@@ -27,14 +53,18 @@ def user_query_service():
     req_home = request.args.get('home', None)
     req_shell = request.args.get('shell', None)
     print req_name, req_uid, req_gid, req_comment, req_home, req_shell
-    return "User Query Service"
+    users_list = read_users_by_query(req_name, req_uid, req_gid, req_comment, req_home, req_shell)
+    return jsonify(users_list)
 
 
 @app.route('/groups/query', methods=['GET'])
 def group_query_service():
-    return "Group Query Service"
+    req_name = request.args.get('name', None)
+    req_gid = request.args.get('gid', None)
+    req_members = request.args.getlist('member', [])
+    group_list = read_group_by_query(req_name, req_gid, req_members)
+    return jsonify(group_list)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- app_lib.py
+++ app_lib.py
@@ -56,3 +56,56 @@ def read_group_file(gfile_name=DEFAULT_GROUP_FILE):
 
     return read_group
 
+
+def read_users_by_query(req_name=None,
+                        req_uid=None,
+                        req_gid=None,
+                        req_comment=None,
+                        req_home=None,
+                        req_shell=None):
+
+    q_users = read_users_file()
+    if req_name:
+        q_users = filter(lambda x: x['name'] == req_name, q_users)
+
+    if req_uid:
+        q_users = filter(lambda x: x['uid'] == req_uid, q_users)
+
+    if req_gid:
+        q_users = filter(lambda x: x['gid'] == req_gid, q_users)
+
+    if req_comment:
+        q_users = filter(lambda x: x['comment'] == req_comment, q_users)
+
+    if req_home:
+        q_users = filter(lambda x: x['home'] == req_home, q_users)
+
+    if req_shell:
+        q_users = filter(lambda x: x['shell'] == req_shell, q_users)
+
+    return q_users
+
+
+def read_group_by_query(req_name=None,
+                        req_gid=None,
+                        req_members=[]):
+
+    q_grps = read_group_file()
+    if req_name:
+        q_grps = filter(lambda x: x['name'] == req_name, q_grps)
+
+    if req_gid:
+        q_grps = filter(lambda x: x['gid'] == req_gid, q_grps)
+
+    if req_members:
+        q_grps = filter(lambda x: set(req_members).issubset(set(x['members'])), q_grps)
+
+    return q_grps
+
+
+def read_group_by_gid_list(req_gid_list):
+
+    q_grps = read_group_file()
+    q_grps = filter(lambda x: x['gid'] in req_gid_list, q_grps)
+
+    return q_grps
